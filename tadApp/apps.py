e0be84c15f41ac3b022f65b92d0145e983cb21f7
EonSyntax <eonsyntax@gmail.com>
2025-07-18 15:44:14 +0100

    tong2

--- tadApp/apps.py
+++ tadApp/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_migrate
 import os
 
 
@@ -14,7 +15,11 @@ class MyAppConfig(AppConfig):
     def ready(self):
         if os.environ.get("CREATE_SUPERUSER", "false").lower() == "true":
             from .utils import create_admin_user
-            try:
-                create_admin_user()
-            except Exception as e:
-                print(f"⚠ Superuser creation failed: {e}")
+
+            def create_user_after_migrate(sender, **kwargs):
+                try:
+                    create_admin_user()
+                except Exception as e:
+                    print(f"⚠ Superuser creation failed: {e}")
+
+            post_migrate.connect(create_user_after_migrate, sender=self)
